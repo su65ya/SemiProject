982fedc88e99ebfd7b933344875aafde3c028818
wonyi <wonyi@DESKTOP-DQF4GBS>
2020-07-08 15:01:29 +0900

    멤버 정보수정 오류 해결

--- SemiTeamProject/src/com/kh/semi/Test.java
+++ SemiTeamProject/src/com/kh/semi/Test.java
@@ -1,5 +1,26 @@
 package com.kh.semi;
 
-public class Test {
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.ParseException;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		String date = "2012년 7월 11일 (수)";
+		
+		SimpleDateFormat original_format = new SimpleDateFormat("yyyy년 M월 d일 (E)");	
+		SimpleDateFormat new_format = new SimpleDateFormat("yyyy-MM-dd");
+		
+	try {
+		Date original_date = original_format.parse(date);
+		String new_date = new_format.format(original_date);
+		System.out.println(new_date);
+	}
+	catch (Exception e) {
+		e.printStackTrace();
+	}
+	}
 }
